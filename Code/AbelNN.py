eb600e15cdd7e8d83defc8385933340d8193cd60
abel-gr <abel.bugatti@hotmail.com>
2021-11-23 18:01:28 +0100

    Create AbelNN.py

---
+++ Code/AbelNN.py
@@ -0,0 +1,15 @@
+
+# Main file of the Abel Neural Networks module
+# Copyright Abel Garcia
+
+
+# You can use each of the .py files separately or you can import
+# this main file if you want to load all files. This .py will
+# also load my AutoML module.
+
+version = 1.2
+
+
+from MLP_Abel import MLP_Abel as MLP
+from ConvNetAbel import ConvNetAbel as ConvNet
+from AutoML_Abel import AutoML_Abel as AutoML
