b4cbb21a685ab5f030618d98f7a0582585af0e31
abel-gr <abel.bugatti@hotmail.com>
2021-12-03 20:19:40 +0100

    MLP import and export procedures

--- Code/MLP_Abel.py
+++ Code/MLP_Abel.py
@@ -10,10 +10,10 @@ import matplotlib.cm as cm
 from pylab import text
 import math
 
-version = 1.2
-
 
 class MLP_Abel:
+
+    version = 1.2
     
     def __init__(self, hidden = [1], nEpochs = 1, learningRate=0.1, manualWeights=[], 
                  debugLevel=1, rangeRandomWeight=None, showLogs=False, softmax=False,
@@ -69,6 +69,8 @@ class MLP_Abel:
         self.iterationDrop = iterationDrop
         
         self.XavierInitialization = '1'
+
+        self.lastLayerNeurons = -1
         
         
     def draw(self, showWeights=False, textSize=9, customRadius=0):
@@ -170,12 +172,23 @@ class MLP_Abel:
         self.showLogs = bool(mConfig[1])
         self.lastLayerNeurons = int(mConfig[2])
         self.numEpochs = int(mConfig[3])
-        self.learningRate = mConfig[4]
+        self.learningRate = float(mConfig[4])
         self.debugMode = int(mConfig[5])
         self.softmax = bool(mConfig[6])
+        self.activationFunction = str(mConfig[7])
+        self.verbose = bool(mConfig[8])
+        self.use = str(mConfig[9])
+        self.batch_size = int(mConfig[10])
+        self.batch_gradient = str(mConfig[11])
+        self.batch_mult = int(mConfig[12])
+        self.dropout = float(mConfig[13])
+        self.pre_norm = bool(mConfig[14])
+        self.shuffle = bool(mConfig[15])
+        self.iterationDrop = float(mConfig[16])
+        self.version_importedModel = mConfig[17]
+        self.hiddenL2 = mConfig[18]
+        self.hiddenL = mConfig[19]
             
-        self.hiddenL2 = np.load(path + filename + '_hiddenLayers2.npy', allow_pickle=True).tolist()
-        self.hiddenL = np.load(path + filename + '_hiddenLayers.npy', allow_pickle=True).tolist()
             
         if self.debugMode > 0:
             
@@ -198,12 +211,24 @@ class MLP_Abel:
         mConfig.append(self.learningRate)
         mConfig.append(self.debugMode)
         mConfig.append(self.softmax)
-        mConfig = np.asarray(mConfig, dtype=np.float32)
+        mConfig.append(self.activationFunction)
+        mConfig.append(self.verbose)
+        mConfig.append(self.use)
+        mConfig.append(self.batch_size)
+        mConfig.append(self.batch_gradient)
+        mConfig.append(self.batch_mult)
+        mConfig.append(self.dropout)
+        mConfig.append(self.pre_norm)
+        mConfig.append(self.shuffle)
+        mConfig.append(self.iterationDrop)
+        mConfig.append(self.version)
+        mConfig.append(self.hiddenL2)
+        mConfig.append(self.hiddenL)
+
+        mConfig = np.asarray(mConfig, dtype=object)
         
         np.save(path + filename + '_config.npy', mConfig)
             
-        np.save(path + filename + '_hiddenLayers2.npy', self.hiddenL2)
-        np.save(path + filename + '_hiddenLayers.npy', self.hiddenL)
             
         if self.debugMode > 0:
             
@@ -212,6 +237,7 @@ class MLP_Abel:
         if self.debugMode > 1:
 
             np.save(path + filename + '_debugWeights.npy', np.asarray(self.debugWeights, dtype=object))
+
         
     def log(self, *m):
         if self.showLogs:
